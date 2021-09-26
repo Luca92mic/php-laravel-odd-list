7ac46a9f98f8af2edd8fd3e60943ea906cddc24c
Luca <luca.mic92@gmail.com>
2021-09-26 11:44:53 +0200

    odd list

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+  
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-success">
+      <div class="container-fluid">
+        <router-link :to="{name: 'home'}" class="navbar-brand">
+          Boolpress
+        </router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item" v-for="(link, index) in menuBoolpress" :key="index">
+              <router-link :to="{ name: link.routeName }" class="nav-link active">
+                {{ link.label }}
+              </router-link>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data(){
+    return {
+      menuBoolpress: [
+        {
+          label: 'Home',
+          routeName: 'home'
+        },
+        {
+          label: 'Chi siamo',
+          routeName: 'about'
+        },
+        {
+          label: 'I miei Posts',
+          routeName: 'post'
+        },
+        {
+          label: 'Contatti',
+          routeName: 'contact'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
